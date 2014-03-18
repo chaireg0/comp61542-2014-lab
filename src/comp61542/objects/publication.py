72d134835f8cf15f9ed51cbb43da320c0984250d
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-03-18 16:57:21 +0000

    Simple test and implementation for publications per year average

---
+++ src/comp61542/objects/publication.py
@@ -0,0 +1,20 @@
+'''
+Created on 18 Mar 2014
+
+@author: vnicolao
+'''
+
+class Publication(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, title):
+        '''
+        Constructor
+        '''
+        self.title = title
+
+
+
