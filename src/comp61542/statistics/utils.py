d797bde1345198557d30ad546c72a7b3e4b29814
Lok Shun Justin Chan <justinchan@justinchan.net>
2014-03-20 10:27:50 +0000

    entered test cases to util.py. Now there are test cases that sort by title, first author and year

--- src/comp61542/statistics/utils.py
+++ src/comp61542/statistics/utils.py
@@ -6,3 +6,10 @@ Created on 18 Mar 2014
 
 def sortPublicationsByFirstAuthors (publist):
     return sorted(publist, key=lambda pub: pub.authors[0]) # sort by author's name
+
+
+def sortPublicationsByYear(publist):
+    return sorted(publist, key=lambda pub: pub.year) # sort by publication's year
+
+def sortPublicationsByTitle(publist):
+    return sorted(publist, key=lambda pub: pub.title) # sort by publication's title
--- test/test_name_last_occurrence.py
+++ test/test_name_last_occurrence.py
@@ -8,6 +8,7 @@ import unittest
 
 from comp61542.objects import academic
 from comp61542.objects.academic import Academic
+
 from comp61542.database import database
 from comp61542.database.database import Publication
 
@@ -15,8 +16,6 @@ from comp61542.statistics import utils
 
 
 class Test(unittest.TestCase):
-
-
 
     def testSortByFirstAuthor(self):
         pub1 = Publication("", "", "", ["Joe Bloggs", "Harry Potter", "Lady Gaga", "John Keane"])
@@ -27,8 +26,29 @@ class Test(unittest.TestCase):
         
         sortedpublist = utils.sortPublicationsByFirstAuthors(publist)
         
-        self.assertEquals([pub3, pub1, pub2], sortedpublist, "pub3 should come first, then pub2, then pub1 instead is " + str(sortedpublist))
+        self.assertEquals([pub3, pub1, pub2], sortedpublist, "pub3 should come first, then pub1, then pub2. However, the result is " + str(sortedpublist))
         
+    def testSortByYear(self):
+        pub1 = Publication("", "", 2008, "")
+        pub2 = Publication("", "", 1993, "")
+        pub3 = Publication("", "", 1999, "")
+        
+        publist = [pub1, pub2, pub3]
+        
+        sortedpublist = utils.sortPublicationsByYear(publist)
+        
+        self.assertEquals([pub2, pub3, pub1], sortedpublist, "pub2 should come first, then pub3, then pub1. However, the result is " + str(sortedpublist))
+
+    def testSortByTitle(self):
+        pub1 = Publication("", "Putin's love with Tzar", "", "")
+        pub2 = Publication("", "High-tech weapon", "", "")
+        pub3 = Publication("", "Nuclear fission", "", "")
+        
+        publist = [pub1, pub2, pub3]
+        
+        sortedpublist = utils.sortPublicationsByTitle(publist)
+        
+        self.assertEquals([pub2, pub3, pub1], sortedpublist, "pub2 should come first, then pub3, then pub1. However, the result is " + str(sortedpublist))
         
 
 if __name__ == "__main__":
